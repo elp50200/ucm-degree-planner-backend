fce8942ed04da63cd838fe7f0ec599313b71189e
elp50200 <elp50200@ucmo.edu>
2023-08-23 19:26:19 -0500

    initial commit

---
+++ src/main/java/com/ucm/degreeplanner/DegreePlannerApplication.java
@@ -0,0 +1,13 @@
+package com.ucm.degreeplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DegreePlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DegreePlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucm/degreeplanner/DegreePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucm.degreeplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DegreePlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
