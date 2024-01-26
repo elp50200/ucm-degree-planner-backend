d47dbbde71ad13a0283827273e92d354cadb2ef4
elp50200 <elp50200@ucmo.edu>
2024-01-26 10:16:18 -0600

    Created all domains, service, controller, and DAO classes as well as starting work on user controller endpoints

---
+++ src/main/java/com/ucm/degreeplanner/repository/ScheduleDAO.java
@@ -0,0 +1,7 @@
+package com.ucm.degreeplanner.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ScheduleDAO {
+}
--- src/main/java/com/ucm/degreeplanner/service/userService.java
+++
@@ -1,4 +0,0 @@
-package com.ucm.degreeplanner.service;
-//this is just a placeholder file
-public class userService {
-}
